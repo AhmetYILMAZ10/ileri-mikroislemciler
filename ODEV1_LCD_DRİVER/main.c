e3b323a7093b9eb2b692d66a0108b01adaf451dc
AhmetYILMAZ10 <ahmet.yilmaz64@ogr.sakarya.edu.tr>
2025-12-29 14:56:37 +0300

    Update main.c

--- ODEV1_LCD_DRİVER/main.c
+++ ODEV1_LCD_DRİVER/main.c
@@ -12,16 +12,17 @@ int main(void)
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ); //Sistem saatini ayarlar: 16MHz harici kristal ve PLL kullanarak işlemciyi 40MHz hızına kurar
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //F Portu çevre birimini aktif eder.
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3); //F Portundaki 1, 2 ve 3. pinleri (RGB LED pinleri) ÇIKIŞ olarak ayarlar
-    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4); //F Portundaki 4. pini Butonu giriş olarak ayarlar
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPU); //Buton için ayar:4mA akım gücü ve Dahili Pull-Up direnci (WPU) tanımlar
     baslangic();
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x02);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x02); //0000 1110 kırmızı ledi aktif eder
 
-    satir_sutun(1,2);
+    satir_sutun(1,2); // LCD imlecini 1. satır, 2. sütun konumuna taşır
     printf("isim soyisim");
-    satir_sutun(2,1);
+    satir_sutun(2,1); //LCD imlecini 2. satır, 1. sütun konumuna taşır
     printf("üniversite /okul no");
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x08);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x08); //0000 1110 yeşil ledi aktif eder
 }
+
 
 
